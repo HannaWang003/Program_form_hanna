37e21ed6528bb794c3b4b1bb00cdb8faab95c9f4
Hanna <hannawang003@gmail.com>
2024-02-21 09:49:10 +0800

    完成圖形縮圖

--- image.php
+++ image.php
@@ -11,7 +11,41 @@
  * 5.輸出檔案
  */
 if (!empty($_FILES['img']['tmp_name'])) {
-    move_uploaded_file($_FILES['img']['tmp_name'], "./imgs/{$_FILES['img']['tmp_name']}");
+    move_uploaded_file($_FILES['img']['tmp_name'], "./imgs/{$_FILES['img']['name']}");
+    $source_path = "./imgs/{$_FILES['img']['name']}";
+    $type = $_FILES['img']['type'];
+    switch ($type) {
+        case "image/jpeg":
+            $source = imagecreatefromjpeg($source_path);
+            list($width, $height) = getimagesize($source_path);
+            break;
+        case "image/png":
+            $source = imagecreatefrompng($source_path);
+            list($width, $height) = getimagesize($source_path);
+            break;
+        case "image/gif":
+            $source = imagecreatefromgif($source_path);
+            list($width, $height) = getimagesize($source_path);
+            break;
+    }
+    $dst_path = "./imgs/small_{$_FILES['img']['name']}";
+    $dst_width = 150;
+    $dst_height = 200;
+    $dst_source = imagecreatetruecolor($dst_width, $dst_height);
+    imagecopyresampled($dst_source, $source, 0, 0, 0, 0, $dst_width, $dst_height, $width, $height);
+    switch ($type) {
+        case "image/jpeg":
+            imagejpeg($dst_source, $dst_path);
+            break;
+        case "image/png":
+            imagepng($dst_source, $dst_path);
+            break;
+        case "image/gif":
+            imagegif($dst_source, $dst_path);
+            break;
+    }
+    imagedestroy($source);
+    imagedestroy($dst_source);
 }
 ?>
 <!DOCTYPE html>
@@ -28,7 +62,7 @@ if (!empty($_FILES['img']['tmp_name'])) {
 <body>
     <h1 class="header">圖形處理練習</h1>
     <!---建立檔案上傳機制--->
-    <form action="?" method='post'>
+    <form action="?" method='post' enctype='multipart/form-data'>
         <label for="">選擇檔案</label>
         <input type="file" name="img" id="">
         <input type="submit" value='上傳'>
@@ -36,7 +70,7 @@ if (!empty($_FILES['img']['tmp_name'])) {
 
 
     <!----縮放圖形----->
-
+    <img src="<?= $dst_path ?>" alt="">
 
     <!----圖形加邊框----->
 
